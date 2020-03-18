086737874b9a559fdfe1731b6be1d04f5348c92f
Chiara <chiara.relevat@oulook.fr>
2020-03-18 12:54:15 +0100

    classe pays fini

--- projet_mobile/app/src/main/java/com/example/projet_mobil/jeux/Pays.java
+++ projet_mobile/app/src/main/java/com/example/projet_mobil/jeux/Pays.java
@@ -5,4 +5,20 @@ Définie le un pays
  ---------------*/
 
 public class Pays {
+    private int image;
+    private String nom;
+
+    public int getImage(){return this.image;}
+    public void setImage(int image){this.image = image;}
+    public String getNom(){return this.nom;}
+    public void setNom(String nom){this.nom = nom;}
+
+    public Pays(int image, String nom){
+        this.image = image;
+        this.nom = nom;
+    }
+
+    public String toString(){
+        return "Image : " + image + ", " + nom;
+    }
 }
